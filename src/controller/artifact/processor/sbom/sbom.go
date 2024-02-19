6d399bd5a16b625e5571320d1621875f40cfd5c4
stonezdj <stonezdj@gmail.com>
2024-02-19 12:31:48 -0300

    get sbom content with additions

--- src/controller/artifact/processor/sbom/sbom.go
+++ src/controller/artifact/processor/sbom/sbom.go
@@ -2,12 +2,16 @@ package sbom
 
 import (
 	"context"
+	"encoding/json"
+	"io"
 
 	"github.com/goharbor/harbor/src/controller/artifact/processor"
 	"github.com/goharbor/harbor/src/controller/artifact/processor/base"
 	"github.com/goharbor/harbor/src/lib/errors"
 	"github.com/goharbor/harbor/src/lib/log"
 	"github.com/goharbor/harbor/src/pkg/artifact"
+
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
 const (
@@ -17,6 +21,7 @@ const (
 
 func init() {
 	pc := &Processor{}
+	pc.ManifestProcessor = base.NewManifestProcessor()
 	if err := processor.Register(pc, mediaType); err != nil {
 		log.Errorf("failed to register processor for media type %s: %v", mediaType, err)
 		return
@@ -28,6 +33,39 @@ type Processor struct {
 }
 
 func (m *Processor) AbstractAddition(ctx context.Context, art *artifact.Artifact, addition string) (*processor.Addition, error) {
-	return nil, errors.New(nil).WithCode(errors.BadRequestCode).
-		WithMessage("debug: addition %s isn't supported for %s", addition, ArtifactTypeSBOM)
+
+	man, _, err := m.RegCli.PullManifest(art.RepositoryName, art.Digest)
+	if err != nil {
+		return nil, errors.New(nil).WithCode(errors.NotFoundCode).WithMessage("The sbom is not found with error %v", err)
+	}
+
+	mediaType, payload, err := man.Payload()
+	if err != nil {
+		return nil, errors.New(nil).WithCode(errors.NotFoundCode).WithMessage("The sbom is not found with error %v", err)
+	}
+	manifest := &v1.Manifest{}
+	if err := json.Unmarshal(payload, manifest); err != nil {
+		return nil, err
+	}
+	for _, layer := range manifest.Layers {
+		// chart do have two layers, one is config, we should resolve the other one.
+		layerDgst := layer.Digest.String()
+		if layerDgst != manifest.Config.Digest.String() {
+			_, blob, err := m.RegCli.PullBlob(art.RepositoryName, layerDgst)
+			if err != nil {
+				return nil, errors.New(nil).WithCode(errors.NotFoundCode).WithMessage("The sbom is not found with error %v", err)
+			}
+			content, err := io.ReadAll(blob)
+			if err != nil {
+				return nil, err
+			}
+			blob.Close()
+			return &processor.Addition{
+				Content:     content,
+				ContentType: mediaType,
+			}, nil
+		}
+	}
+	return nil, errors.New(nil).WithCode(errors.NotFoundCode).WithMessage("The sbom is not found")
+
 }
